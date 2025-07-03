c3417f49a77613153535c4b6d2865f22f81ab9a1
shansitads <shansitads@gmail.com>
2025-07-03 01:15:20 +0000

    Deploying to gh-pages from @ shansitads/shansitads.github.io@35722ebda7c2beb0a47373bd9e4db559eb37b11a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,12 +41,7 @@ ninja.data = [{
             window.location.href = "/blog/2025/ML-Papers-In-10-Minutes/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
